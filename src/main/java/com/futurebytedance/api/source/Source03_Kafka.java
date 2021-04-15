4670c8e42f3cf039e0e948aa504c40658fc10556
sssv587 <1092663295@qq.com>
2021-04-15 22:40:50 +0800

    Kafka

---
+++ src/main/java/com/futurebytedance/api/source/Source03_Kafka.java
@@ -0,0 +1,36 @@
+package com.futurebytedance.api.source;
+
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer011;
+
+import java.util.Properties;
+
+/**
+ * @author yuhang.sun
+ * @version 1.0
+ * @date 2021/4/15 - 22:05
+ * @Description FlinkAPI-source-从kafka中读取数据
+ */
+public class Source03_Kafka {
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+
+        Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers", "localhost:9092");
+        properties.setProperty("group.id", "consumer-group");
+        properties.setProperty("key.deserializer",
+                "org.apache.kafka.common.serialization.StringDeserializer");
+        properties.setProperty("value.deserializer",
+                "org.apache.kafka.common.serialization.StringDeserializer");
+        properties.setProperty("auto.offset.reset", "latest");
+
+        DataStreamSource<String> dataStream = env.addSource(new FlinkKafkaConsumer011<>("sensor", new SimpleStringSchema(), properties));
+
+        dataStream.print();
+
+        env.execute();
+    }
+}
