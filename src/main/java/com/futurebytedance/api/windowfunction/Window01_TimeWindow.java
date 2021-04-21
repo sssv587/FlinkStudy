3e7a275736dcc66be4ca25125e46d3234dcde157
sssv587 <1092663295@qq.com>
2021-04-21 16:25:42 +0800

    TimeWindow

--- src/main/java/com/futurebytedance/api/windowfunction/Window01_TimeWindow.java
+++ src/main/java/com/futurebytedance/api/windowfunction/Window01_TimeWindow.java
@@ -1,17 +1,24 @@
 package com.futurebytedance.api.windowfunction;
 
 import com.futurebytedance.api.beans.SensorReading;
+import org.apache.commons.collections.IteratorUtils;
 import org.apache.flink.api.common.functions.AggregateFunction;
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.common.functions.ReduceFunction;
+import org.apache.flink.api.java.tuple.Tuple;
+import org.apache.flink.api.java.tuple.Tuple3;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.windowing.ProcessWindowFunction;
+import org.apache.flink.streaming.api.functions.windowing.WindowFunction;
 import org.apache.flink.streaming.api.windowing.assigners.EventTimeSessionWindows;
 import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows;
 import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows;
 import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
+import org.apache.flink.util.Collector;
 
 /**
  * @author yuhang.sun
@@ -36,6 +43,7 @@ public class Window01_TimeWindow {
         });
 
         // 开窗测试
+        //1.增量聚合函数
         DataStream<Integer> resultStream = dataStream.keyBy("id")
 //                .countWindow(10,2);
 //                .window(EventTimeSessionWindows.withGap(Time.minutes(1)));
@@ -64,7 +72,23 @@ public class Window01_TimeWindow {
                            }
                 );
 
+        //2.全窗口函数
+        SingleOutputStreamOperator<Tuple3<String, Long, Integer>> resultStream2 = dataStream.keyBy("id")
+                .timeWindow(Time.seconds(15))
+//                .process(new ProcessWindowFunction<SensorReading, Object, Tuple, TimeWindow>() {
+//                })
+                .apply(new WindowFunction<SensorReading, Tuple3<String, Long, Integer>, Tuple, TimeWindow>() {
+                    @Override
+                    public void apply(Tuple tuple, TimeWindow window, Iterable<SensorReading> input, Collector<Tuple3<String, Long, Integer>> out) throws Exception {
+                        String id = tuple.getField(0);
+                        Long windowEnd = window.getEnd();
+                        Integer count = IteratorUtils.toList(input.iterator()).size();
+                        out.collect(new Tuple3<>(id, windowEnd, count));
+                    }
+                });
+
         resultStream.print();
+        resultStream2.print();
 
         env.execute();
     }
