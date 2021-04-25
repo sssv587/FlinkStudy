7925d7739cb0cf9999ffed872f2b1bf4d724d723
sssv587 <1092663295@qq.com>
2021-04-26 00:33:37 +0800

    StateBackend

---
+++ src/main/java/com/futurebytedance/api/state/State04_FaultTolerance.java
@@ -0,0 +1,41 @@
+package com.futurebytedance.api.state;
+
+import com.futurebytedance.api.beans.SensorReading;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.contrib.streaming.state.RocksDBStateBackend;
+import org.apache.flink.runtime.state.filesystem.FsStateBackend;
+import org.apache.flink.runtime.state.memory.MemoryStateBackend;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+/**
+ * @author yuhang.sun
+ * @version 1.0
+ * @date 2021/4/26 - 0:27
+ * @Description
+ */
+public class State04_FaultTolerance {
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+
+        // 1.状态后端配置
+        env.setStateBackend(new MemoryStateBackend());
+        // env.setStateBackend(new FsStateBackend());
+        // env.setStateBackend(new RocksDBStateBackend());
+
+        //socket文本流
+        DataStreamSource<String> inputStream = env.socketTextStream("localhost", 7777);
+
+        //转换成SensorReading类型
+        DataStream<SensorReading> dataStream = inputStream.map((MapFunction<String, SensorReading>) value -> {
+            String[] fields = value.split(",");
+            return new SensorReading(fields[0], Long.valueOf(fields[1]), Double.valueOf(fields[2]));
+        });
+
+        dataStream.print();
+
+        env.execute();
+    }
+}
