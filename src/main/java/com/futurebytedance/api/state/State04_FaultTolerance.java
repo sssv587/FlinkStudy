539ed277daa96fc0aeb334cf8cefe7a26bd6532e
sssv587 <1092663295@qq.com>
2021-04-27 01:59:11 +0800

    FaultTolerance

--- src/main/java/com/futurebytedance/api/state/State04_FaultTolerance.java
+++ src/main/java/com/futurebytedance/api/state/State04_FaultTolerance.java
@@ -2,9 +2,13 @@ package com.futurebytedance.api.state;
 
 import com.futurebytedance.api.beans.SensorReading;
 import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.restartstrategy.RestartStrategies;
+import org.apache.flink.api.common.time.Time;
 import org.apache.flink.contrib.streaming.state.RocksDBStateBackend;
+import org.apache.flink.runtime.executiongraph.restart.RestartStrategy;
 import org.apache.flink.runtime.state.filesystem.FsStateBackend;
 import org.apache.flink.runtime.state.memory.MemoryStateBackend;
+import org.apache.flink.streaming.api.CheckpointingMode;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
@@ -24,6 +28,23 @@ public class State04_FaultTolerance {
         env.setStateBackend(new MemoryStateBackend());
         // env.setStateBackend(new FsStateBackend());
         // env.setStateBackend(new RocksDBStateBackend());
+
+        // 2.检查点配置
+        env.enableCheckpointing(300);
+
+        // 高级选项
+        env.getCheckpointConfig().setCheckpointingMode(CheckpointingMode.EXACTLY_ONCE);
+        env.getCheckpointConfig().setCheckpointTimeout(60000L);
+        env.getCheckpointConfig().setMaxConcurrentCheckpoints(2);
+        env.getCheckpointConfig().setMinPauseBetweenCheckpoints(100);
+        env.getCheckpointConfig().setPreferCheckpointForRecovery(true);
+        env.getCheckpointConfig().setTolerableCheckpointFailureNumber(10);
+
+        // 3.重启策略配置
+        // 固定延迟重启
+        env.setRestartStrategy(RestartStrategies.fixedDelayRestart(3, 1000L));
+        // 失败率重启
+        env.setRestartStrategy(RestartStrategies.failureRateRestart(3, Time.minutes(10), Time.minutes(1)));
 
         //socket文本流
         DataStreamSource<String> inputStream = env.socketTextStream("localhost", 7777);
